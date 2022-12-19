848a0b6ede545a3deac1f50449c99101544b14ff
Rafidimas17 <rafidimas1706@gmail.com>
2022-12-20 05:33:01 +0700

    upload today

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,25 +1,28 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 // import InputNumber from "elements/Form/InputNumber";
 // import Breadcrumb from "elements/Breadcrumb";
 // import InputDate from "elements/Form/InputDate";
 // import Dropdown from "elements/Dropdown";
-export default class Example extends Component {
-  render() {
-    // const breadcrumb = [
-    //   { pageTitle: "Home", pageHref: "" },
-    //   { pageTitle: "Mountain Details", pageHref: "" },
-    // ];
-    return (
-      <div className="container">
-        <div
-          className="row align-items-center justify-content-center"
-          style={{ height: "100vh" }}
-        >
-          <div className="col-auto">
-            {/* <Dropdown /> */}
-          </div>
+import Status from "parts/Status";
+import Tombol from "parts/Tombol";
+import Effect from "parts/Effect";
+export default function Example() {
+  // const [getNavValue, setNavValue] = useState("");
+  // const clicked = () => {
+  //   setNavValue("Haloo");
+  // };
+  return (
+    <div className="container">
+      <div
+        className="row align-items-center justify-content-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="col-auto">
+          {/*<Status navValue={getNavValue} />
+          <Tombol clicked={clicked} />**/}
+          <Effect />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
